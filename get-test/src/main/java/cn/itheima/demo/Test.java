3632b88a956ffbcc09e84ee40a3ea1b0fd725dba
tao <ltt30660539@qq.com>
2019-01-07 20:16:49 +0800

    修改好了

--- get-test/src/main/java/cn/itheima/demo/Test.java
+++ get-test/src/main/java/cn/itheima/demo/Test.java
@@ -5,10 +5,10 @@ public class Test {
         System.out.println ("我是第一次添加的!");
         System.out.println ("我是第二次添加的!");
         System.out.println ("我是第同步到github添加的!");
-<<<<<<< HEAD
+
 System.out.println ("我是master分支同步到github添加的!");
-=======
+
  System.out.println ("我是分支devv同步到 github添加的!");
->>>>>>> dev
+
     }
 }
