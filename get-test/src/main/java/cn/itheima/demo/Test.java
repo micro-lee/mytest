7e998038a85d812b3a7e78ca6f21ab1e29e7b18a
tao <ltt30660539@qq.com>
2019-01-07 18:35:00 +0800

    我修改了,同步到了github

--- get-test/src/main/java/cn/itheima/demo/Test.java
+++ get-test/src/main/java/cn/itheima/demo/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println ("我是第一次添加的!");
         System.out.println ("我是第二次添加的!");
+        System.out.println ("我是第同步到github添加的!");
     }
 }
