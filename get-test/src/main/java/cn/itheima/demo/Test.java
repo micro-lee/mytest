55a0b2097e5e8b874bc5a41d0b0efce8fa13ab4f
tao <ltt30660539@qq.com>
2019-01-07 18:31:30 +0800

    提交到仓库

--- get-test/src/main/java/cn/itheima/demo/Test.java
+++ get-test/src/main/java/cn/itheima/demo/Test.java
@@ -1,7 +1,8 @@
-package cn.itheima.demo;
+﻿package cn.itheima.demo;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println ("我是第一次添加的!");
+        System.out.println ("我是第二次添加的!");
     }
 }
